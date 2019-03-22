0c9450ecb761adedc5a84a523370581ad6999e73
siguhe <siguhe@stud.ntnu.no>
2019-03-22 17:12:16 +0100

    Added a whatchdog for orders

--- statemachines/util_communication/kennel.d
+++ statemachines/util_communication/kennel.d
@@ -1,26 +1,11 @@
-bool order_watchdog(int ID, int order){ //TODO: Define types for order and ID.
-	import std.concurrency;
-	import std.datetime;
-	bool answer;
-	Duration interval =10.msecs; 
-	while(true){
-		receiveTimeout(interval,
-			(bool ack){ answer=ack;},
-			(Variant rest){
-				import std.string;
-				throw(new StringException("Whatchdog recieved invalid type of message."));
-			}
-		);
-		if(answer==true){
-			return true; //If confirmed order, die silently. 
-		}
-		udp_send_safe(order, thisTid);
-		receiveTimeout(
-			
-		);
-		//TODO: Send order (Aka do a bid) again, with fine attached.
-		//TODO: Listen for ack. If noone took the order, that is: if udp says "no ack" =>  Start function over again.
-		//TODO: If someone took the order: die.
-		//TODO: Is internal and external different now?
-	}
+void order_watchdog(CallButton order, int timeout_sec, Tid order_list_tid ){ //TODO: Define types for order and ID.
+	import std.concurrency, std.datetime, std.conv;
+	udp_send_safe(order, thisTid);
+	receiveTimeout((timeout_sec*1000).msecs,
+		(ConfirmedOrder c){ 
+			return;
+		},	
+	);
+	order_list_tid.send(order);
+	return;
 }
